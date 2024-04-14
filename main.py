5d10e5f313a65229ee708857015450181932cb03
johanaanez <janez@fi.uba.ar>
2024-04-14 19:08:38 -0300

    reading args from arguments

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import time
+import args
 from laundry.laundry_factory import LaundryFactory
 
 def main():
     # obtención de argumentos
     start_time = time.time()
+    arguments = args.parse_arguments()
     input_file = "laundry/primer_problema.txt"
     output_file = "primer_problema.txt"
+    #input_file = arguments.inputfile
+    #output_file = arguments.outputfile
 
     # cargar datos de los lavados
     laundry = LaundryFactory.Load(input_file)
---
+++ run_example.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 ./main.py -if laundry/primer_problema.txt -of primer_problema.txt
